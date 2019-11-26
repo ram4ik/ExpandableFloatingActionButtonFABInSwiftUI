9d848b1f458121e3369a4e99a62e5fc7effb0e63
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-26 21:30:03 +0200

    Expandable Floating Action Button FAB In SwiftUI

--- ExpandableFloatingActionButtonFABInSwiftUI/ContentView.swift
+++ ExpandableFloatingActionButtonFABInSwiftUI/ContentView.swift
@@ -9,13 +9,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            List(0..<25) { i in
+                Text("mainview \(i)")
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    ExpandableFAB(show: $show).padding(.trailing, 30)
+                }.padding([.bottom], 20)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExpandableFAB: View {
+    @Binding var show: Bool
+    var body: some View {
+        VStack(spacing: 20) {
+            if self.show {
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Image(systemName: "tv.fill")
+                        .resizable()
+                        .frame(width: 35, height: 25)
+                        .padding(18)
+                }
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Image(systemName: "cart.fill")
+                        .resizable()
+                        .frame(width: 35, height: 25)
+                        .padding(18)
+                }
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+            }
+            Button(action: {
+                self.show.toggle()
+            }) {
+                Image(systemName: "chevron.up")
+                    .resizable()
+                    .frame(width: 25, height: 15)
+                    .padding(22)
+            }
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotationEffect(.init(degrees: self.show ? 180 : 0))
+        }.animation(.spring())
     }
 }
